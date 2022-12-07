ae257e037d287b5a607861f93a703fe940ca6404
M4RT4Z4K <116658532+M4RT4Z4K@users.noreply.github.com>
2022-12-07 20:52:40 +0100

    Update foo.hpp

--- foo.hpp
+++ foo.hpp
@@ -7,6 +7,16 @@
 
 std::vector< char > foo(std::list< Human >& people)
 {
-    // Twoja implementacja tutaj
-    return {};
+    std::vector< char >          animallover;
+    std::vector< int >::iterator it = people.begin();
+    for (it; it < people.end(); it++) {
+        *it.birthday();
+        if (*it.isMonster()) {
+            animallover.push_front("n");
+        }
+        else {
+            animallover.push_front("y");
+        }
+    }
+    return (animallover);
 }
